b7c1bee7461b3cec495bd796b2a422fa7d55a8b2
arumhatre <133888887+arumhatre@users.noreply.github.com>
2025-04-24 14:24:03 +0530

    add

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,195 @@
-var isMobileOrTablet=window.innerWidth<1024;$(".icon_banner_foot_slider").owlCarousel({loop:!0,responsiveClass:!0,nav:!0,margin:5,autoplay:isMobileOrTablet,autoplayTimeout:6e3,smartSpeed:400,navText:["<img src='images/previmage.webp' alt='001 prev' width='50' height='50' />","<img src='images/nextimage.webp' alt='001 next' width='50' height='50' />"],responsive:{0:{items:1},600:{items:1},768:{items:2},1024:{items:3},1200:{items:3}}}),$(".screenshot_slider").owlCarousel({loop:!0,responsiveClass:!0,nav:!0,margin:5,autoplay:!0,autoplayTimeout:6e3,smartSpeed:400,navText:["<img src='images/previmage.webp' alt='001 prev' width='50' height='50' />","<img src='images/nextimage.webp' alt='001 next' width='50' height='50' />"],responsive:{0:{items:1},600:{items:1},768:{items:2},1024:{items:2},1200:{items:3}}}),$("#testimonilas_slider").owlCarousel({loop:!1,responsiveClass:!0,nav:!0,margin:20,autoplay:!1,autoplayTimeout:4e3,smartSpeed:400,navText:["<img src='images/previmage.webp' alt='002 prev' width='50' height='50' />","<img src='images/nextimage.webp' alt='002 next' width='50' height='50' />"],responsive:{0:{items:1},600:{items:1},768:{items:2},1024:{items:3},1200:{items:3}}}),$("#advance_skin_slider").owlCarousel({loop:!0,responsiveClass:!0,nav:!0,margin:0,autoplay:!0,autoplayTimeout:8e3,smartSpeed:400,navText:["<img src='images/previmage.webp' alt='003 prev' width='50' height='50' />","<img src='images/nextimage.webp' alt='003 next' width='50' height='50' />"],responsive:{0:{items:1},600:{items:1},768:{items:1},1024:{items:1},1200:{items:1}}}),$("#googlerating_slider").owlCarousel({loop:!0,responsiveClass:!0,nav:!0,margin:0,autoplay:!0,autoplayTimeout:8e3,smartSpeed:400,navText:["<img src='images/previmage.webp' alt='004 prev' width='50' height='50' />","<img src='images/nextimage.webp' alt='004 next' width='50' height='50' />"],responsive:{0:{items:1},600:{items:1},768:{items:2},1024:{items:3},1200:{items:3}}}),$(".owl-nav .owl-prev").attr("role","button"),$(".owl-nav .owl-next").attr("role","button");const scrollToTopBtn=document.querySelector(".scrollToTopBtn"),rootElement=document.documentElement;function handleScroll(){const e=rootElement.scrollHeight-rootElement.clientHeight;scrollToTopBtn.classList.toggle("showBtn",rootElement.scrollTop/e>.02)}function scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}function openForm(){document.getElementById("myForm").style.display="block",$("#myForm .myForm-top-form").append($(".form-top-new")),$(".contactFormHolder .contactForm").hide()}function closeForm(){$(".contactFormHolder .contactForm").show().append($(".form-top-new")),document.getElementById("myForm").style.display="none"}scrollToTopBtn?.addEventListener("click",scrollToTop),window.addEventListener("scroll",handleScroll),$((function(){$(".popup-youtube, .popup-vimeo").magnificPopup({type:"iframe",mainClass:"mfp-fade",removalDelay:160,preloader:!1,fixedContentPos:!0})}));const togglers=document.querySelectorAll("[data-bs-toggle]");togglers.forEach((e=>{const t=document.querySelector(e.dataset.bsToggle);e.classList.contains("active")&&(t.style.maxHeight=t.scrollHeight+"px",e.closest(".accordion__item").classList.add("active")),e.addEventListener("click",(e=>{const t=e.currentTarget,o=document.querySelector(t.dataset.bsToggle);t.classList.contains("active")?(o.style.maxHeight="",t.closest(".accordion__item").classList.remove("active")):(o.style.maxHeight=o.scrollHeight+"px",t.closest(".accordion__item").classList.add("active")),t.classList.toggle("active"),document.querySelectorAll(".accordion__header").forEach((e=>{if(e!==t){e.classList.remove("active");const t=document.querySelector(e.dataset.bsToggle);t&&(t.style.maxHeight="",e.closest(".accordion__item").classList.remove("active"))}}))}))}));
+var isMobileOrTablet = window.innerWidth < 1024;
+$(".icon_banner_foot_slider").owlCarousel({
+	loop: !0,
+	responsiveClass: !0,
+	nav: !0,
+	margin: 5,
+	autoplay: isMobileOrTablet,
+	autoplayTimeout: 6e3,
+	smartSpeed: 400,
+	navText: ["<img src='images/previmage.webp' alt='001 prev' width='50' height='50' />", "<img src='images/nextimage.webp' alt='001 next' width='50' height='50' />"],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 1
+		},
+		768: {
+			items: 2
+		},
+		1024: {
+			items: 3
+		},
+		1200: {
+			items: 3
+		}
+	}
+}), $(".screenshot_slider").owlCarousel({
+	loop: !0,
+	responsiveClass: !0,
+	nav: !0,
+	margin: 5,
+	autoplay: !0,
+	autoplayTimeout: 6e3,
+	smartSpeed: 400,
+	navText: ["<img src='images/previmage.webp' alt='001 prev' width='50' height='50' />", "<img src='images/nextimage.webp' alt='001 next' width='50' height='50' />"],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 1
+		},
+		768: {
+			items: 2
+		},
+		1024: {
+			items: 2
+		},
+		1200: {
+			items: 3
+		}
+	}
+}), $("#testimonilas_slider").owlCarousel({
+	loop: !1,
+	responsiveClass: !0,
+	nav: !0,
+	margin: 20,
+	autoplay: !1,
+	autoplayTimeout: 4e3,
+	smartSpeed: 400,
+	navText: ["<img src='images/previmage.webp' alt='002 prev' width='50' height='50' />", "<img src='images/nextimage.webp' alt='002 next' width='50' height='50' />"],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 1
+		},
+		768: {
+			items: 2
+		},
+		1024: {
+			items: 3
+		},
+		1200: {
+			items: 3
+		}
+	}
+}), $("#advance_skin_slider").owlCarousel({
+	loop: !0,
+	responsiveClass: !0,
+	nav: !0,
+	margin: 0,
+	autoplay: !0,
+	autoplayTimeout: 8e3,
+	smartSpeed: 400,
+	navText: ["<img src='images/previmage.webp' alt='003 prev' width='50' height='50' />", "<img src='images/nextimage.webp' alt='003 next' width='50' height='50' />"],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 1
+		},
+		768: {
+			items: 1
+		},
+		1024: {
+			items: 1
+		},
+		1200: {
+			items: 1
+		}
+	}
+}), $("#googlerating_slider").owlCarousel({
+	loop: !0,
+	responsiveClass: !0,
+	nav: !0,
+	margin: 0,
+	autoplay: !0,
+	autoplayTimeout: 8e3,
+	smartSpeed: 400,
+	navText: ["<img src='images/previmage.webp' alt='004 prev' width='50' height='50' />", "<img src='images/nextimage.webp' alt='004 next' width='50' height='50' />"],
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 1
+		},
+		768: {
+			items: 2
+		},
+		1024: {
+			items: 3
+		},
+		1200: {
+			items: 3
+		}
+	}
+}), $(".owl-nav .owl-prev").attr("role", "button"), $(".owl-nav .owl-next").attr("role", "button");
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn"),
+	rootElement = document.documentElement;
+
+function handleScroll() {
+	const e = rootElement.scrollHeight - rootElement.clientHeight;
+	scrollToTopBtn.classList.toggle("showBtn", rootElement.scrollTop / e > .02)
+}
+
+function scrollToTop() {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	})
+}
+
+function openForm() {
+	document.getElementById("myForm").style.display = "block", $("#myForm .myForm-top-form").append($(".form-top-new")), $(".contactFormHolder .contactForm").hide()
+}
+
+function closeForm() {
+	$(".contactFormHolder .contactForm").show().append($(".form-top-new")), document.getElementById("myForm").style.display = "none"
+}
+scrollToTopBtn?.addEventListener("click", scrollToTop), window.addEventListener("scroll", handleScroll), $((function() {
+	$(".popup-vimeo").magnificPopup({
+		type: "iframe",
+		mainClass: "mfp-fade",
+		removalDelay: 160,
+		preloader: !1,
+		fixedContentPos: !0
+	})
+}));
+$(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+      type: 'iframe',
+      iframe: {
+        patterns: {
+          youtube: {
+            index: 'youtube.com/embed/',
+            id: function(url) { return url; }, // use full embed URL directly
+            src: '%id%'
+          }
+        }
+      },
+      mainClass: 'mfp-fade',
+      preloader: false,
+      fixedContentPos: false
+    });
+  });
+const togglers = document.querySelectorAll("[data-bs-toggle]");
+togglers.forEach((e => {
+	const t = document.querySelector(e.dataset.bsToggle);
+	e.classList.contains("active") && (t.style.maxHeight = t.scrollHeight + "px", e.closest(".accordion__item").classList.add("active")), e.addEventListener("click", (e => {
+		const t = e.currentTarget,
+			o = document.querySelector(t.dataset.bsToggle);
+		t.classList.contains("active") ? (o.style.maxHeight = "", t.closest(".accordion__item").classList.remove("active")) : (o.style.maxHeight = o.scrollHeight + "px", t.closest(".accordion__item").classList.add("active")), t.classList.toggle("active"), document.querySelectorAll(".accordion__header").forEach((e => {
+			if (e !== t) {
+				e.classList.remove("active");
+				const t = document.querySelector(e.dataset.bsToggle);
+				t && (t.style.maxHeight = "", e.closest(".accordion__item").classList.remove("active"))
+			}
+		}))
+	}))
+}));
